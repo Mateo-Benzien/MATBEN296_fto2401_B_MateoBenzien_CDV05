31ba9266ac9768a76467bd8a2a9468ccc24cfea3
Mateo Benzien <156769785+Mateo-Benzien@users.noreply.github.com>
2024-05-31 14:04:07 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+    // Here you can add more functionality to send the form data to your email or a server
+});
